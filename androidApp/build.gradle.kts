0e8e567ad6dc6d59bed3ba230d569e5d85260d5d
Alpár Szotyori <sz.alpar@gmail.com>
2022-08-25 10:23:42 +0200

    chore(androidApp): Update dependencies

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -6,13 +6,6 @@ plugins {
 group = "net.repeatuntil.wishly"
 version = "1.0"
 
-dependencies {
-    implementation(project(":shared"))
-    implementation("com.google.android.material:material:1.2.1")
-    implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-}
-
 android {
     compileSdk = 32
     defaultConfig {
@@ -27,4 +20,11 @@ android {
             isMinifyEnabled = false
         }
     }
+}
+
+dependencies {
+    implementation(project(":shared"))
+    implementation("com.google.android.material:material:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.5.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
